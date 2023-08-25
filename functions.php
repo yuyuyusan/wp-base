1a8c5892b4cd45a2080703333937dd1b6c16d4a5
yuyuyusan <youzhiqichuan@gmail.com>
2023-08-25 17:47:50 +0900

    [update] functions 変更

--- functions.php
+++ functions.php
@@ -152,3 +152,25 @@ function my_custom_post_type_permalinks_set($termlink, $term, $taxonomy)
 	return str_replace('/' . $taxonomy . '/', '/', $termlink);
 }
 add_filter('term_link', 'my_custom_post_type_permalinks_set', 11, 3);
+
+// ------------------------------
+// wp_nav_menuのliにclass追加
+// ------------------------------
+function add_additional_class_on_li($classes, $item, $args)
+{
+  if (isset($args->add_li_class)) {
+    $classes['class'] = $args->add_li_class;
+  }
+  return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+// wp_nav_menuのaにclass追加
+function add_additional_class_on_a($classes, $item, $args)
+{
+  if (isset($args->add_li_class)) {
+    $classes['class'] = $args->add_a_class;
+  }
+  return $classes;
+}
+add_filter('nav_menu_link_attributes', 'add_additional_class_on_a', 1, 3);
